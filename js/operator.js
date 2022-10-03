8004759e5fd9f42fa1245b8a398c265a4d55306f
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-03 20:53:11 +0530

    discussed operator

---
+++ js/operator.js
@@ -0,0 +1,20 @@
+// let a=1;
+// let b=2;
+let a = 1,
+  b = 2;
+let sum = a + b;
+
+2 * 2 * 2 * 2;
+Math.pow(2, 4);
+2 ** 4;
+
+let someNumber = 5;
+let remainder = someNumber % 2;
+
+someNumber=someNumber+1;
+someNumber++;
+someNumber+=1;
+
+let val1=val2=val3=3;
+
+
