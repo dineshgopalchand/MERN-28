9a5750ed88d1d7d6338ccbf2df08d9255e553db4
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-03 21:59:06 +0530

    While loop

---
+++ js/loop.js
@@ -0,0 +1,16 @@
+// While Loop
+let count=11;
+let limit=10;
+while (count<=limit) {
+    console.log( count);
+    count++;
+}
+
+// initilization
+// while(condition){
+//     // update
+// }
+
+
+
+
