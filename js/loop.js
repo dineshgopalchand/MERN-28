8d7b3ec9c0c521187f4e9fa84ac7ca12121b5d60
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-03 22:01:52 +0530

    do while introduced

--- js/loop.js
+++ js/loop.js
@@ -1,15 +1,26 @@
-// While Loop
-let count=11;
-let limit=10;
-while (count<=limit) {
-    console.log( count);
-    count++;
-}
+// // While Loop
+// let count=11;
+// let limit=10;
+// while (count<=limit) {
+//     console.log( count);
+//     count++;
+// }
 
 // initilization
 // while(condition){
 //     // update
 // }
+
+// do-while loop
+let count=12;
+let limit=10;
+do {
+    console.log( count);
+    count++;
+}while (count<=limit)
+
+// by default it will loop once, doesn't matter condition is true or false
+
 
 
 
