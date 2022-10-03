8238922d5b7888fc0d7b5188165078338dc5242e
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-03 22:19:03 +0530

    For loop introduced + assignment

--- js/loop.js
+++ js/loop.js
@@ -12,14 +12,52 @@
 // }
 
 // do-while loop
-let count=12;
-let limit=10;
-do {
-    console.log( count);
-    count++;
-}while (count<=limit)
+// let count=12;
+// let limit=10;
+// do {
+//     console.log( count);
+//     count++;
+// }while (count<=limit)
 
-// by default it will loop once, doesn't matter condition is true or false
+// // by default it will loop once, doesn't matter condition is true or false
+
+
+// For Loop
+// for(init; condition; update){
+//     // statement
+// }
+for (let count = 0; count <= 10; count++) {
+  const p4val=Math.pow(count,4);
+  console.log(p4val);
+    
+}
+// for(;;){
+//     console.log('something');
+//     // return false;
+// }
+
+// for(let first of second) {third}
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+// array.forEach(element => {
+    
+// });
+
+// Assignment
+// 1. check given number is palindrom
+//     121
+//     123454321
+
+// 2. febonacci series upto 100 element
+//     0,1,1,2,3,5,8,13,21..
+
+// 3. check a number is prime or not
+
+
 
 
 
