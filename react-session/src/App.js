8f5734c9df357fe767b8c20c162ac8bbfea70fc8
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-19 21:33:54 +0530

    React project setup done cmd: npx create-react-app <project-name>

---
+++ react-session/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>
+        React Application
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
