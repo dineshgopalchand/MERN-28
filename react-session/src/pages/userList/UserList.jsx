1f3d8df831c243593e11447b85ab11d989975e87
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-10-28 21:28:44 +0530

    introduced key in map to avoid error

--- react-session/src/pages/userList/UserList.jsx
+++ react-session/src/pages/userList/UserList.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import Profile from "../profile/Profile";
-import './UserList.css';
+import "./UserList.css";
 
 const UserList = () => {
-    const userDetailList=[
-        {
-            "login": "brahmareddy123",
-            "avatar_url": "https://avatars.githubusercontent.com/u/33050494?v=4"
-        },
-        {
-            "login": "KirtiBihari",
-            "avatar_url": "https://avatars.githubusercontent.com/u/39719334?v=4"
-        },
-        {
-            "login": "rohithcvijayan",
-            "avatar_url": "https://avatars.githubusercontent.com/u/49511065?v=4"
-        },
-        {
-            "login": "rohithcvijayan",
-            "avatar_url": "https://avatars.githubusercontent.com/u/49511065?v=4"
-        },
-        {
-          "login": "KirtiBihari1",
-          "avatar_url": "https://avatars.githubusercontent.com/u/39719334?v=4"
-      }
-    ];
+  const userDetailList = [
+    {
+      login: "brahmareddy123",
+      avatar_url: "https://avatars.githubusercontent.com/u/33050494?v=4",
+    },
+    {
+      login: "KirtiBihari",
+      avatar_url: "https://avatars.githubusercontent.com/u/39719334?v=4",
+    },
+    {
+      login: "rohithcvijayan",
+      avatar_url: "https://avatars.githubusercontent.com/u/49511065?v=4",
+    },
+    {
+      login: "rohithcvijayan1",
+      avatar_url: "https://avatars.githubusercontent.com/u/49511065?v=4",
+    },
+    {
+      login: "KirtiBihari1",
+      avatar_url: "https://avatars.githubusercontent.com/u/39719334?v=4",
+    },
+  ];
   return (
     <div className="flex">
-      {
-        userDetailList.map((userDetails)=>{
-          return <Profile user={userDetails}/>
-        })
-      }
+      {userDetailList.map((userDetails) => {
+        return <Profile user={userDetails} key={userDetails.login} />;
+      })}
     </div>
   );
 };
