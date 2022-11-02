c5f96aa3877b511123bad5ceddf55397a0de9f86
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-02 22:12:23 +0530

    Login form with single useState

--- react-session/src/pages/login/Login.jsx
+++ react-session/src/pages/login/Login.jsx
@@ -2,28 +2,53 @@ import { useState } from "react";
 import Input from "../../UI/input/Input";
 import "./Login.css";
 const Login = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [userNameErr, setUserNameErr] = useState(false);
-  const [passwordErr, setPasswordErr] = useState(false);
-  const userNameHandler = (event) => {
+  const [formVal, setFormVal] = useState({
+    username: {
+      value: "",
+      error: false,
+    },
+    password: {
+      value: "",
+      error: false,
+    },
+    formError: true,
+  });
+  const formFieldHandler = (event) => {
     const value = event.target.value;
-    // if(value.trim().length<=2){
-    //   setUserNameErr(true)
-    // }else{
-    //   setUserNameErr(false)
-    // }
-    setUserNameErr(value.trim().length < 5);
-    setUserName(value);
+    const name = event.target.name;
+    console.log(value, name);
+    let error = false;
+    if (name === "username") {
+      error = value.trim().length < 5;
+    } else if (name === "password") {
+      error = value.trim().length < 7;
+    }
+    setFormVal((prev) => {
+      return {
+        ...prev,
+        [name]: {
+          value,
+          error,
+        },
+      };
+    });
+    setFormVal((prev) => {
+      return { ...prev, formError: isFormError(prev) };
+    });
   };
-  const passwordHandler = (event) => {
-    const value = event.target.value;
-    setPasswordErr(value.trim().length < 7);
-    setPassword(value);
+
+  const isFormError = ({ username, password }) => {
+    return (
+      username.error ||
+      password.error ||
+      username.value === "" ||
+      password.value === ""
+    );
   };
+
   const loginFormHandler = (event) => {
     event.preventDefault();
-    console.log("form value", { userName }, { password });
+    console.log("form value", formVal.username.value, formVal.password.value);
   };
   return (
     <div className="form-box">
@@ -31,36 +56,27 @@ const Login = () => {
         <h2>Login Form</h2>
 
         <Input
-          onChange={userNameHandler}
-          className={`${userNameErr ? "error-field" : ""}`}
+          onChange={formFieldHandler}
+          className={`${formVal.username.error ? "error-field" : ""}`}
           field={{
             type: "text",
             name: "username",
-            value: userName,
+            value: formVal.username.value,
             autoComplete: "false",
           }}
         />
         <Input
-          onChange={passwordHandler}
-          className={`${passwordErr ? "error-field" : ""}`}
+          onChange={formFieldHandler}
+          className={`${formVal.password.error ? "error-field" : ""}`}
           field={{
             type: "password",
             name: "password",
-            value: password,
+            value: formVal.password.value,
             autoComplete: "false",
           }}
         />
         <div className="form-group">
-          <button
-            disabled={
-              userNameErr ||
-              passwordErr ||
-              userName.length === 0 ||
-              password.length === 0
-            }
-          >
-            Sign In
-          </button>
+          <button disabled={formVal.formError}>Sign In</button>
         </div>
       </form>
     </div>
