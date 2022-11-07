ea48d3553586d6e706629587172c7b11f2b286dd
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-07 22:21:23 +0530

    introduced react-icons library  for icon

--- react-session/src/UI/header/Header.jsx
+++ react-session/src/UI/header/Header.jsx
@@ -1,11 +1,15 @@
 import "./Header.css";
+import { BsBookFill, BsBook } from "react-icons/bs";
 function Header() {
   return (
     <>
       <header className="header-element">
-        <h2>React Session</h2>
+        <h2>
+          <BsBookFill />
+          React Session
+          <BsBook />
+        </h2>
       </header>
-
     </>
   );
 }
