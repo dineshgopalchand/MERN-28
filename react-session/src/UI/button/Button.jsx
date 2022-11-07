841852ff27d17e89e0d2d94c6260fe7c2418f707
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-07 21:34:28 +0530

    inline css added on button

--- react-session/src/UI/button/Button.jsx
+++ react-session/src/UI/button/Button.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { memo } from "react";
 
 const Button = (props) => {
-    console.log(props);
+  console.log(props);
   const buttonStyle = {
     backgroundColor: "blue",
     color: "#ffffff",
     borderRadius: "5px",
   };
-  return <button style={buttonStyle} {...props}>{props.children}</button>;
+  return (
+    <>
+      {/* <button style={buttonStyle} {...props}> */}
+      <button
+        style={{
+          backgroundColor: "blue",
+          color: "#ffffff",
+          borderRadius: "5px",
+        }}
+        {...props}
+      >
+        {props.children}
+      </button>
+    </>
+  );
 };
 
-export default Button;
+export default memo(Button);
