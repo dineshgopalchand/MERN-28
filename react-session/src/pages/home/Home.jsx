8ebf0d7921e1e4c6920a964caa9ea87456548ba0
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-11 22:43:12 +0530

    Disussed React Fragment

--- react-session/src/pages/home/Home.jsx
+++ react-session/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Container } from "react-bootstrap";
 import Experties from "../../features/home/Experties";
 import Slider from "../../features/home/Slider";
 import "./Home.css";
 const Home = () => {
+  const list = ['dinesh','alok', 'khan sir', 'varun', 'Manisha'].map((item, index) => {
+    return {
+      key: index,
+      value: item,
+    };
+  });
   return (
     <>
       <Container fluid className="px-0">
@@ -12,6 +18,16 @@ const Home = () => {
       <Container fluid className="px-0 mt-2">
         <Experties />
       </Container>
+      <dl>
+        {list.map((item) => {
+          return (
+            <Fragment key={item.key}>
+              <dt >{item.key}</dt>
+              <dd>{item.value}</dd>
+            </Fragment>
+          );
+        })}
+      </dl>
     </>
   );
 };
