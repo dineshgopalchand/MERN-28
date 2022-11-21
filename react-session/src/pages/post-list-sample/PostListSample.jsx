564271e97286fa2474ee3ae4e1c8ed946fa9460c
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-21 23:05:42 +0530

    Introduced UseReducer Hook

--- react-session/src/pages/post-list-sample/PostListSample.jsx
+++ react-session/src/pages/post-list-sample/PostListSample.jsx
@@ -1,22 +1,46 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+
+const SET_TITLE = "set_title";
+const SET_DESC = "set_description";
+const SET_INIT_VAL = "reset_value";
 const initFormVal = {
   title: "",
   desc: "",
 };
+
+const formReducer = (state, action) => {
+  //   console.log(state, action);
+  switch (action.type) {
+    case SET_TITLE:
+      return { ...state, title: action.payload };
+    case SET_DESC:
+      return { ...state, desc: action.payload };
+    case SET_INIT_VAL:
+      return initFormVal;
+    default:
+      return state;
+  }
+};
+
 const PostListSample = () => {
   const [postList, setPostList] = useState([]);
-  const [formVal, setFormVal] = useState(initFormVal);
-  const formFieldHandler = (event) => {
-    const { name, value } = event.target;
-    setFormVal((prev) => {
-      return { ...prev, [name]: value };
-    });
-  };
+  //   const [formVal, setFormVal] = useState(initFormVal);
+
+  const [formValue, formDispatch] = useReducer(formReducer, initFormVal);
+
+  //   const formFieldHandler = (event) => {
+  //     const { name, value } = event.target;
+  //     setFormVal((prev) => {
+  //       return { ...prev, [name]: value };
+  //     });
+  //   };
   const newPostSubmitHandler = (event) => {
     event.preventDefault();
-    setPostList((prev) => [...prev, formVal]);
-    setFormVal(initFormVal);
+    // setPostList((prev) => [...prev, formVal]);
+    setPostList((prev) => [...prev, formValue]);
+    // setFormVal(initFormVal);
+    formDispatch({ type: SET_INIT_VAL });
   };
   return (
     <>
@@ -28,10 +52,17 @@ const PostListSample = () => {
                 <input
                   type="text"
                   name="title"
-                  value={formVal.title}
+                  //   value={formVal.title}
+                  value={formValue.title}
                   className="form-control"
                   placeholder="Enter Title"
-                  onChange={formFieldHandler}
+                  //   onChange={formFieldHandler}
+                  onChange={(event) => {
+                    formDispatch({
+                      type: SET_TITLE,
+                      payload: event.target.value,
+                    });
+                  }}
                 />
               </div>
               <div className="form-group">
@@ -39,10 +70,17 @@ const PostListSample = () => {
                   id=""
                   rows="5"
                   name="desc"
-                  value={formVal.desc}
+                  //   value={formVal.desc}
+                  value={formValue.desc}
                   placeholder="Give some details"
                   className="form-control"
-                  onChange={formFieldHandler}
+                  //   onChange={formFieldHandler}
+                  onChange={(event) => {
+                    formDispatch({
+                      type: SET_DESC,
+                      payload: event.target.value,
+                    });
+                  }}
                 ></textarea>
               </div>
               <div className="form-group">
