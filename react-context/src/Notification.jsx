0dbddf25716ab01f9883f2f461c797a8c2680671
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-11-24 21:51:10 +0530

    Removed Context consumer and used useContext hook

--- react-context/src/Notification.jsx
+++ react-context/src/Notification.jsx
@@ -1,34 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NotificationContext, Notification_ACTION } from "./App";
 import NotificationList from "./NotificationList";
 
 const Notification = () => {
+  // const notificationCtx = useContext(NotificationContext);
+  // const notificationDispatch = notificationCtx.notificationDispatch;
+  const { notificationDispatch } = useContext(NotificationContext);
+  const addNotification = () => {
+    notificationDispatch({
+      type: Notification_ACTION.add,
+      payload: {
+        id: Date.now(),
+        title: "notication" + Math.round(Math.random() * 10),
+        seen: false,
+      },
+    });
+  };
   return (
-    <NotificationContext.Consumer>
-      {(notificationCtx) => {
-        const notificationDispatch = notificationCtx.notificationDispatch;
-        return (
-          <div className="bordered">
-            Notification
-            <button
-              onClick={() => {
-                notificationDispatch({
-                  type: Notification_ACTION.add,
-                  payload: {
-                    id: Date.now(),
-                    title: "notication" + Math.round(Math.random() * 10),
-                    seen: false,
-                  },
-                });
-              }}
-            >
-              Add
-            </button>
-            <NotificationList />
-          </div>
-        );
-      }}
-    </NotificationContext.Consumer>
+    <div className="bordered">
+      Notification
+      <button onClick={addNotification}>Add</button>
+      <NotificationList />
+    </div>
   );
 };
 
--- react-context/src/NotificationList.jsx
+++ react-context/src/NotificationList.jsx
@@ -1,41 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NotificationContext, Notification_ACTION } from "./App";
 
 const NotificationList = () => {
+  const { list: notificationList, notificationDispatch } =
+    useContext(NotificationContext);
   return (
-    <NotificationContext.Consumer>
-      {(notificationCtx) => {
-        // console.log(notificationCtx);
-        const notificationList = notificationCtx.list;
-        const notificationDispatch = notificationCtx.notificationDispatch;
-        return (
-          <div className="bordered">
-            {notificationCtx.list.length > 0 ? (
-              <ul>
-                {notificationList.map((item) => {
-                  return (
-                    <li
-                      key={item.id}
-                      className={`notification-list ${!item.seen && "bold"} `}
-                      onClick={() =>
-                        notificationDispatch({
-                          type: Notification_ACTION.update,
-                          payload: item,
-                        })
-                      }
-                    >
-                      {item.title}
-                    </li>
-                  );
-                })}
-              </ul>
-            ) : (
-              <p>No Notification Available</p>
-            )}
-          </div>
-        );
-      }}
-    </NotificationContext.Consumer>
+    <div className="bordered">
+      {notificationList.length > 0 ? (
+        <ul>
+          {notificationList.map((item) => {
+            return (
+              <li
+                key={item.id}
+                className={`notification-list ${!item.seen && "bold"} `}
+                onClick={() =>
+                  notificationDispatch({
+                    type: Notification_ACTION.update,
+                    payload: item,
+                  })
+                }
+              >
+                {item.title}
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No Notification Available</p>
+      )}
+    </div>
   );
 };
 
