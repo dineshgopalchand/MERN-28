9cb56d85529cc917da1ac1bfe06f873be65e5ba2
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-12-07 22:39:47 +0530

    Restructured Menu in Header

--- react-router-redux/src/layout/Header.jsx
+++ react-router-redux/src/layout/Header.jsx
@@ -1,37 +1,57 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const navLink=[
+    {
+      content:'Home',
+      link:'/'
+    },
+    {
+      content:'About',
+      link:'/about'
+    },
+    {
+      content:'Contact',
+      link:'/contact'
+    },
+    {
+      content:'Services',
+      link:'/services'
+    },
+    {
+      content:'Article',
+      link:'/article'
+    }
+  ]
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {
+                navLink.map((nav,index)=>(
+                  <li className="nav-item" key={index}>
+                  <NavLink className="nav-link " aria-current="page" to={nav.link}>
+                    {nav.content}
+                  </NavLink>
+                </li>
+                ))
+              }
+            </ul>
+        
           </Nav>
           <Form className="d-flex">
             <Form.Control
