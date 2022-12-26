cddda0b8514b938aaa14a4cb4c5a040921b9b13a
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2022-12-26 22:16:46 +0530

    Basic website with 5 route

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,20 @@
 const http = require("http");
 const PORT = 8000;
 const app = http.createServer((request, response) => {
+  const { url, method } = request;
+  console.log({ url, method });
   response.setHeader("Content-type", "text/html");
-  response.end("Hello world");
+  if (url == "/") {
+    response.end("<h2>Home page</h2>");
+  } else if (url == "/contact") {
+    response.end("<h2>Contact page</h2>");
+  } else if (url == "/about") {
+    response.end("<h2>About page</h2>");
+  } else if (url == "/blog") {
+    response.end("<h2>Blog page</h2>");
+  } else {
+    response.end("<h2>404</h2>");
+  }
 });
 app.listen(PORT, () => {
   console.log(`Server started : http://localhost:${PORT}`);
