e9ba0c5b5456bf244321dfe20ac6c235448487ad
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-02 22:15:14 +0530

    Discussed HTTP request GET method also used JS to add field dynamically

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,27 +1,37 @@
 const http = require("http");
 const fs = require("fs");
 const PORT = 8000;
+const HOST = `http://localhost:${PORT}`;
 const app = http.createServer((request, response) => {
   const { url, method } = request;
-  console.log({ url, method });
+  const { pathname, searchParams } = new URL(HOST + url);
   response.setHeader("Content-type", "text/html");
   response.setHeader("status", 201);
-  if (url == "/") {
-    const homePageContent=fs.readFile('./public/html/index.html',(err,data)=>{
-        response.end(data);
+  if (pathname == "/") {
+    const homePageContent = fs.readFile('./public/html/index.html', (err, data) => {
+      response.end(data);
     });
 
-    
-  } else if (url == "/contact") {
-    response.end("<h2>Contact page</h2>");
-  } else if (url == "/about") {
+  } else if (pathname == "/contact") {
+    if (method == 'POST') {
+      console.log('post methode content');
+      response.end("<h2>post method</h2>");
+    } else {
+      console.log('GET methode content',searchParams);
+      const fname=searchParams.get('fname');
+      console.log(fname)
+      fs.readFile('./public/html/contact.html', (err, data) => {
+        response.end(data);
+      });
+    }
+  } else if (pathname == "/about") {
     response.end("<h2>About page</h2>");
-  } else if (url == "/blog") {
+  } else if (pathname == "/blog") {
     response.end("<h2>Blog page</h2>");
   } else {
     response.end("<h2>404</h2>");
   }
 });
 app.listen(PORT, () => {
-  console.log(`Server started : http://localhost:${PORT}`);
+  console.log(`Server started : ${HOST}`);
 });
