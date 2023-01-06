cf25f240d0f861381d2ae75bff412ddca0de1b89
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-06 22:51:56 +0530

    installed and used express for get and post method req

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,51 @@
-console.log('first js file ');
-const numberList=[1,2,34,3,2,2,2,4,2,.21,434,7887];
-const sortedData=numberList.sort((a,b)=>a<b?-1:1);
-console.log(sortedData)
-process.stdout.write(JSON.stringify(sortedData))
-numberList.forEach(item=>console.log(item*2))
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const PORT = 8001;
+const HOST = `http://localhost:${PORT}`;
+const fs = require('fs');
 
-// console.log(process);
-// process.stdin.on('data', data => {
-//     console.log(`You typed ${data.toString()}`);
-//     process.exit();
-//   });
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>');
+})
+app.get('/post', (req, res) => {
+    const data={
+        postname: 'MERN demand'
+    };
+    res.send(data);
+});
+app.get('/product-list', (req, res) => {
+    let rawdata = fs.readFileSync('./mock-data/product-list.json');
+    const productList=JSON.parse(rawdata);
+    res.send(productList);
+});
+app.get('/contact', (req, res) => {
+    res.send(`
+    <h2>Contact page</h2>
+    <form method="post">
+        <input type="text" name="name" placeholder="Enter name"/>
+        <button>Submit</button>
+    </form>
+    `);
+});
+app.post('/contact',(req,res)=>{
+    let body = req.body;
+    const formattedBody=`
+    <h2>Thank you for submitting form</h2>
+    <p> Submitted name : ${body.name}</p> 
+    `
+    res.send(formattedBody);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server started : ${HOST}`);
+});
+
