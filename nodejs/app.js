d795c94852c800ee1323dd834041d0191a03fe69
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-09 20:47:08 +0530

    discussed custom middleware

--- nodejs/app.js
+++ nodejs/app.js
@@ -12,18 +12,31 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
+// custom middleware
+app.use((req, res, next) => {
+    console.log('logging', req.url);
+    res.setHeader('requested-by', 'dinesh');
+    res.cookie('name','diosjdfj')
+    if(req.url=='/'){
+        req.somedata='MERN class';
+    }
+    next();
+})
+
 app.get('/', (req, res) => {
+    console.log(req.somedata);
     res.send('<h1>Home page</h1>');
 })
 app.get('/post', (req, res) => {
-    const data={
+    const data = {
         postname: 'MERN demand'
     };
     res.send(data);
 });
 app.get('/product-list', (req, res) => {
+    console.log(req.somedata);
     let rawdata = fs.readFileSync('./mock-data/product-list.json');
-    const productList=JSON.parse(rawdata);
+    const productList = JSON.parse(rawdata);
     res.send(productList);
 });
 app.get('/contact', (req, res) => {
@@ -35,9 +48,9 @@ app.get('/contact', (req, res) => {
     </form>
     `);
 });
-app.post('/contact',(req,res)=>{
+app.post('/contact', (req, res) => {
     let body = req.body;
-    const formattedBody=`
+    const formattedBody = `
     <h2>Thank you for submitting form</h2>
     <p> Submitted name : ${body.name}</p> 
     `
