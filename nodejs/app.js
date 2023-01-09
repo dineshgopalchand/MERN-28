f38b7f5505d796abac2b45bcab24cdcfd829510a
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-01-09 21:40:46 +0530

    Discussed req param to fetch dynamic data from URL

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const PORT = 8001;
 const HOST = `http://localhost:${PORT}`;
 const fs = require('fs');
+var morgan = require('morgan');
 
 
 // parse application/x-www-form-urlencoded
@@ -12,13 +13,25 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
 
+// morgan middleware
+// app.use(morgan(function (tokens, req, res) {
+//     return [
+//         tokens.method(req, res),
+//         tokens.url(req, res),
+//         tokens.status(req, res),
+//         tokens.res(req, res, 'content-length'), '-',
+//         tokens['response-time'](req, res), 'ms'
+//     ].join(' ')
+// }));
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
+
 // custom middleware
 app.use((req, res, next) => {
     console.log('logging', req.url);
     res.setHeader('requested-by', 'dinesh');
-    res.cookie('name','diosjdfj')
-    if(req.url=='/'){
-        req.somedata='MERN class';
+    res.cookie('name', 'diosjdfj')
+    if (req.url == '/') {
+        req.somedata = 'MERN class';
     }
     next();
 })
@@ -27,17 +40,28 @@ app.get('/', (req, res) => {
     console.log(req.somedata);
     res.send('<h1>Home page</h1>');
 })
-app.get('/post', (req, res) => {
+app.get('/post/', (req, res) => {
     const data = {
         postname: 'MERN demand'
     };
     res.send(data);
 });
-app.get('/product-list', (req, res) => {
+
+app.get('/product/list', (req, res) => {
     console.log(req.somedata);
+
     let rawdata = fs.readFileSync('./mock-data/product-list.json');
     const productList = JSON.parse(rawdata);
     res.send(productList);
+});
+app.get('/product/:productid', (req, res) => {
+    console.log(req.somedata);
+    const { productid } = req.params;
+
+    let rawdata = fs.readFileSync('./mock-data/product-list.json');
+    const productList = JSON.parse(rawdata);
+    const productDetail = productList.products.filter(p => p.id == productid)[0];
+    res.send(productDetail);
 });
 app.get('/contact', (req, res) => {
     res.send(`
